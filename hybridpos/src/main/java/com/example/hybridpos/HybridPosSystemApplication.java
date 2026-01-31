ba78aea5f1c83721608e319b752650d4b66f4809
Limtotage <abdullahltgyalim@gmail.com>
2026-01-31 13:18:42 +0300

    Project Generated

---
+++ hybridpos/src/main/java/com/example/hybridpos/HybridPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.hybridpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridPosSystemApplication.class, args);
+	}
+
+}
---
+++ hybridpos/src/test/java/com/example/hybridpos/HybridPosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hybridpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridPosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
